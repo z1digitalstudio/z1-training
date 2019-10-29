b1ed3eec6154e4fb1c42aaca20ff607b21e0ae85
egalan26 <eduardo@z1.digital>
2019-10-29 09:52:30 +0100

    feat/navBar

--- src/components/LayoutOne/index.tsx
+++ src/components/LayoutOne/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import globalStyles from '@/styles/global.scss';
 import styles from './styles.module.scss';
+import { Link } from '../Link/Link';
 
 export function LayoutOne() {
   return (
     <>
       <div className={globalStyles.contentWrapper}>
-        <h1 className={styles.title}>Soy el layout 1</h1>
+        <div className={styles.navBar}>
+          <img src="flow-logo-mono.svg" alt="logo" />
+          <Link>Why Flow</Link>
+          <Link>Overview</Link>
+          <Link>Solutions</Link>
+          <Link>Pricing</Link>
+          <Link>More</Link>
+          <Link className={styles.loginItem}>Login</Link>
+        </div>
       </div>
       <div className={styles.background} />
       <div className={globalStyles.contentWrapper}>
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Link(props: any) {
+  return <span {...props}>{props.children}</span>;
+}
