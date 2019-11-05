821aa7983a4dfd5e7c936fe2b28c748e42b0b731
egalan26 <eduardo@z1.digital>
2019-11-05 13:37:52 +0100

    fix/arreglos

--- src/components/TierBox/TierBox.tsx
+++ src/components/TierBox/TierBox.tsx
@@ -4,7 +4,12 @@ import styles from './TierBox.scss';
 export default function TierBox(props: any) {
   return (
     <div className={styles.tierBoxContent}>
-      <div className={styles.tierBox}>{props.children}</div>
+      <div className={styles.tierBox}>
+        {React.Children.map(props.children, (child, i) => {
+          return child;
+        })}
+      </div>
+
       <div className={styles.compareFeatures}>
         {' '}
         Compare all Features <img src="caret-down-solid.svg"></img>
